0c275fe9ee2b848f658f011b07e5c0d1dffd0677
EC2 Default User <ec2-user@ip-172-31-93-207.ec2.internal>
2021-09-13 15:40:09 +0000

    update build file

--- BUILD.sh
+++ BUILD.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-docker-compose down;
+sudo docker image prune -f
+
+sudo docker-compose down;
 
 sudo docker network create nlp_project_default;
 
 git pull;
 
-sudo docker-compose build --build-arg NTC_TOKEN="$(echo $NTC_TOKEN)";
+sudo docker-compose build --no-cache --build-arg NTC_TOKEN="$(echo $NTC_TOKEN)";
 
-sudo docker-compose up -b; 
+sudo docker-compose up -d; 
