bf5fd674616f34e65ff684b0dfb1a78e0b9de990
Wirks <1277554484197005@mil>
2021-09-27 14:33:21 -0400

    updated mongodb; cleaned folder structure

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ import threading
 
 from main import app
 
+app = app.run(host='0.0.0.0', port=8000, debug=True, use_reloader=False)
+
 if __name__ == '__main__':
-    threading.Thread(target=lambda: app.run(host='0.0.0.0', port=8000, debug=True, use_reloader=False)).start()
+    threading.Thread(target=lambda: app).start()
