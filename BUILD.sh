46a3dd1e63d41c5c944aafef4fc2a35fb155f538
cwirks01 <cwirks01@gmail.com>
2021-11-17 09:43:18 -0500

    removed ssl to debug

--- BUILD.sh
+++ BUILD.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo docker image prune -f
-sudo docker volume prune -f
-
 sudo docker-compose down;
+
+sudo docker system prune -f
+sudo docker volume prune -f
 
 sudo docker network create nlp_project_default;
 
-git pull;
+sudo git pull;
 
 sudo docker-compose build --no-cache;
 
