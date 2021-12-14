87c55a9000659313203a7ca9eb03574c7ac45359
Internetuser <Internetuser>
2021-12-14 11:20:33 -0500

    update download option

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from werkzeug.wrappers import Response
 from flask import Flask, render_template, request, flash, redirect, Markup
 from flask_pymongo import PyMongo
 
-MONGO_DB_USERNAME = os.environ['MONGO_DB_USERNAME']
-MONGO_DB_PASSWORD = os.environ['MONGO_DB_PASSWORD']
-MONGO_HOST = "mongodb"
-MONGO_PORT = "27017"
+# MONGO_DB_USERNAME = os.environ['MONGO_DB_USERNAME']
+# MONGO_DB_PASSWORD = os.environ['MONGO_DB_PASSWORD']
+# MONGO_HOST = "mongodb"
+# MONGO_PORT = "27017"
 
-# # DEBUGING
-# MONGO_DB_USERNAME = "root"
-# MONGO_DB_PASSWORD = "password"
-# MONGO_HOST = "23.23.40.32"
-# MONGO_PORT = "27019"
+# DEBUGING
+MONGO_DB_USERNAME = "root"
+MONGO_DB_PASSWORD = "password"
+MONGO_HOST = "23.23.40.32"
+MONGO_PORT = "27019"
 
 MONGO_NLP_DB = "NLP_db"
 MONGO_USER_DB = "users_db"
@@ -169,7 +169,8 @@ def downloaded_file_db(filename):
     if filename.endswith("html"):
         file_out = Markup(file_out)
     elif filename.endswith("json"):
-        file_out = file_out
+        file_out = file_out.seek(0)
+        file_out = file_out.read()
     elif filename.endswith("anx"):
         file_out = file_out
     else:
