d8dc72dfabd2b6bdd48ab26e4a65b2ad0a72c5ee
Internetuser <Internetuser>
2021-12-14 12:28:01 -0500

    update download option

--- Lib/spacy_sent_connections.py
+++ Lib/spacy_sent_connections.py
@@ -398,7 +398,7 @@ class spacy_sent_connections:
         if filename == "data.csv":
             file_out = df_data.to_csv(index=False)
         elif filename == "data.json":
-            file_out = file_in
+            file_out = pd.json_normalize(file_in)
         elif filename == "data.anx":
             chart = pyanx.Pyanx()
             try:
