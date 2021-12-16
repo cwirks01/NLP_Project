8faa058b74d4b01847bb733cac3cbeab0db66d78
cwirks01 <cwirks01@gmail.com>
2021-12-16 15:18:07 -0500

    updated server issues for swell

--- Lib/chart_network.py
+++ Lib/chart_network.py
@@ -105,8 +105,7 @@ def online_network_analysis(df_anb=None, num_of_occurrence=2):
                             showlegend=False,
                             autosize=True,
                             hovermode='closest',
-                            margin="auto",
-                            # margin=dict(b=20, l=5, r=5, t=40),
+                            margin=dict(b=20, l=5, r=5, t=40),
                             annotations=[dict(
                                 text="",
                                 showarrow=False,
